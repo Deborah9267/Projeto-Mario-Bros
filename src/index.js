b9ab1b4f3b78ddc97c3a8c70c6de43067c673c2e
Deborah Carvalho <dehram2018@gmail.com>
2023-01-30 17:45:35 -0300

    medida da Tela ajustada

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-const botao=document.querySelector(".botao");
 
+const botao = document.querySelector('iframe .botao')
+const botaofecharmodal = document.querySelector(".fechar-modal");
 const modal = document.querySelector(".modal");
-const botaoFecharModal = document.querySelector(".fecharmodal");
-
-const linkdovideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
-const video = document.getElementById("video");
-console.log("https://www.youtube.com/embed/Cb4WV4aXBpk");
-
-botao.addEventListener("Click",( ) => {   
-    console.log("clicou no botao");
-    
-    modal.classList.add("aberto");
-    video.SetAttribute("src","https://www.youtube.com/embed/Cb4WV4aXBpk");
-;
-});
+const video = document.querySelector(".video");
+const LinkDoVideo = 'https://www.youtube.com/embed/Cb4WV4aXBpk';
 
 
-botaoFecharModal.addEventListener("Click", () => {
-    modal.classList.remove("aberto");
-    video.SetAttribute("src", "Linkdovideo");
-});
+// CRIEI UMA FUNÇÃO QUE SE COMUNICA COM O BOTÃO DO 'VER TRAILER' CHAMADO abrirVideo()
+function abrirVideo() {
+
+    // criei uma variavel que vai procurar se existe a classe aberto e vamos aos condicionais
+    let procurar = document.querySelector('.aberto')
+
+    //o condicional abaixo esta perguntando, foi encontrado a variavel aberto? como ainda não tem, ele vai achar o valor nulo
+    if (procurar == null) {
+        //como encontrou o valor nulo, ele fez a ação, adicionou aberto ao modal
+        modal.classList.add("aberto");
+
+        ;
+    };
+}
 
 
+//e criei uma funcionalidade para o botão X igual ao botão do trailer
+function fechar() {
+    // criei uma variavel que vai procurar se existe a classe aberto e vamos aos condicionais
+    let procurar = document.querySelector('.aberto')
+
+    //o condicional abaixo esta perguntando, foi encontrado a variavel aberto? como a resposta é verdadeira ele vai pra ação
+    if (procurar = true) {
+        // a ação é tirar a classe aberto
+        modal.classList.remove("aberto");
+        //infelizmente não consegui tirar o som do video, mas já ficou muito bom
+    }
+}
 
